76bab1f23088be26460b6fc11e6a5fe99f12e975
Michal Bohuslávek <mbohuslavek@gmail.com>
2018-07-13 15:11:32 +0200

    Add simple diff output as example test

---
+++ example_test.go
@@ -0,0 +1,41 @@
+package diff_test
+
+import (
+	"fmt"
+
+	"github.com/mibk/diff"
+)
+
+func Example_simpleDiffOutput() {
+	a := []int{'A', 'B', 'C', 'A', 'B', 'B', 'A', 'D'}
+	b := []int{'C', 'B', 'A', 'B', 'A', 'C', 'D'}
+	eds := diff.Diff(a, b)
+
+	var i int
+	for _, e := range eds {
+		for ; i < e.Index; i++ {
+			fmt.Printf(" %c\n", a[i])
+		}
+		if e.Op == diff.Delete {
+			fmt.Printf("-%c\n", a[i])
+			i++
+		} else {
+			fmt.Printf("+%c\n", b[e.Arg])
+		}
+	}
+	for ; i < len(a); i++ {
+		fmt.Printf(" %c\n", a[i])
+	}
+
+	// output:
+	// -A
+	// -B
+	//  C
+	// +B
+	//  A
+	//  B
+	// -B
+	//  A
+	// +C
+	//  D
+}
